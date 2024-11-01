1da321ca6b4d1fa60e26c3d796bf026e1781bea3
wesleyburnawan <wesley.burnawan@gmail.com>
2024-11-01 00:42:09 -0400

    created book model

---
+++ models/Book.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const BookSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+  },
+  pages: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  dateStarted: { type: Date },
+  dateFinished: { type: Date },
+  date: { type: Date, default: Date.now },
+});
+module.exports = mongoose.model("book", BookSchema);
