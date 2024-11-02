da6d6817b22339ea867fefe1f91be540a292324c
Arda Celik <arda.celik@uoit.net>
2024-11-01 23:18:02 -0400

    cleanup and deployment stuff

--- server.js
+++ server.js
@@ -3,16 +3,24 @@ const connectToMongoDB = require("./config/db-connection");
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
 const booksoute = require("./routes/books");
+const path = require("path");
 
 const app = express();
 
 connectToMongoDB();
 app.use(express.json({ extended: false }));
-app.get("/", (req, res) => res.json({ msg: "hello" }));
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/books", booksoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`API is running on ${PORT}`));
